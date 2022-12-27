ef9b59dd48ec903dd4f2617fb8ad41ef7d3a0b2f
dimitri <korgalidzed@gmail.com>
2022-12-27 22:39:13 +0400

    add new code to transfer.sh

--- transfer.sh
+++ transfer.sh
@@ -2,53 +2,44 @@
 
 currentVersion="0.0.1"
 
-httpSingleUpload()
+multiUpload ()
 {
-    response=$(curl -A curl --upload-file "$1" "https://transfer.sh/$2") || { echo "Failure!"; return 1;}
-}
-
-printUploadResponse()
-{
-fileID=$(echo "$response" | cut -d "/" -f 4)
-  cat <<EOF
-Transfer File URL: $response
-EOF
-}
-
-singleUpload()
-{
-  filePath=$(echo "$1" | sed s:"~":"$HOME":g)
-  if ! -f "$filePath" ;then { echo "Error: invalid file path"; return 1;}; fi
-  tempFileName=$(echo "$1" | sed "s/.*\///")
-  echo "Uploading $tempFileName"
-  httpSingleUpload "$filePath $tempFileName"
-}
-
-singleUpload "$1" || exit 1
-printUploadResponse
 
 for file in "$@"; do 
   if   test -f "$file"; then
-     curl --upload-file "$file" "https://transfer.sh/$file" 
- echo " "
+    file_url=$(curl --upload-file "$file" "https://transfer.sh/$file") 
+    curl --upload-file "$file" "https://transfer.sh/$file" &> /dev/null
+filePath=$(echo "$file_url")  
+fileID="$file"
+
+echo "Uploading $fileID"
+echo "Transfer File URL: $filePath"
 
   fi
 done
+}
 
-
-singleDowload () {
+singleDownload() {
 
  if [ "$1" = "-d" ]; then
-   curl -O "https://transfer.sh/$2"
+   curl "$2"  &> /dev/null
  fi
  
-return "printDownloadResponse"
+}
+
+printDownloadResponse()
+{
+ if [ "$1" = "-d" ]; then
+  filepath="$2"
+  file_name=$(echo "$filepath" | cut -d "/" -f 5)
+  echo "Downloading $file_name"  
+ fi
 
 }
 
 
-
-
+help_bar()
+{
  if [ "$1" = "-h" ]; then
 echo "Description: Bash tool to transfer files from the command line."
 echo "Usage:"
@@ -60,6 +51,7 @@ echo "./transfer.sh test.txt "
 echo "./transfer.sh test.txt test2.txt"
 echo "./transfer.sh -d test2.txt"
   elif [ "$1" = "-v" ]; then
-echo "0.0.1"
+echo "$currentVersion"
 
  fi
+}
